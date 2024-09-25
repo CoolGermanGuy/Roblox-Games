862a5275e1db7394d75ff7e6ea60e9aa1a6b07b3
skid205 <EmailCGG@protonmail.com>
2024-09-25 14:19:06 +0200

    Update Map.lua

--- Movement System/ReplicatedStorage/Map.lua
+++ Movement System/ReplicatedStorage/Map.lua
@@ -1,11 +1,11 @@
 -- Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 -- Requirements
-local Object = require(ReplicatedStorage.Object)
+local Object = require(ReplicatedStorage:WaitForChild("Object"))
 
 -- Init
 local Map = {}
-
+Map.__index = Map
 -- New Map
 function Map.New(size: Vector3)
 	local newMap = {}
@@ -17,16 +17,25 @@ function Map.New(size: Vector3)
 		for y = 1, size.Y do
 			newMap[x][y] = {}
 			for z = 1, size.Z do
-				newMap[x][y][z] = {
-					["position"] = Vector3.new(size.X + (8*x), size.Y + (8*y), size.Z + (8*z)),
-					["part"] = Object.New("Air", Vector3.new(size.X + (8*x), size.Y + (8*y), size.Z + (8*z)), {}),
-					--Object.New("Air", Vector3.new(size.X + (8*x), size.Y + (8*y), size.Z + (8*z)), {})
-				}
+				newMap[x][y][z] = {}
+				newMap[x][y][z].Position = Vector3.new(size.X + (8*x), size.Y + (8*y), size.Z + (8*z))
+				newMap[x][y][z].Part = Object.New("Air", Vector3.new(size.X + (8*x), size.Y + (8*y), size.Z + (8*z)), {})
 			end
 		end
 	end
 	setmetatable(newMap, Map)
 	return newMap
+end
+
+-- Functions
+function Map:SetObject(x: int, y: int, z: int, Object: Object)
+	self[x][y][z].Part = Object
+	Object.Position = self[x][y][z].Position
+	Object.Part.Position = Object.Position
+end
+
+function Map:RemoveObject()
+	print("okay")
 end
 
 return Map
