4bdadc617a8ad073c9c290e607728027e61e971a
skid205 <EmailCGG@protonmail.com>
2024-09-26 20:34:22 +0200

    Update EpicScriptName.lua

--- Movement System/ServerScriptService/EpicScriptName.lua
+++ Movement System/ServerScriptService/EpicScriptName.lua
@@ -4,15 +4,12 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Object = require(ReplicatedStorage:WaitForChild("Object"))
 local Map = require(ReplicatedStorage:WaitForChild("Map"))
 
-local testMap = Map:New(Vector3.new(10, 4, 10))
-print(testMap)
-
+local testMap = Map:New(Vector3.new(0, 0, 0), Vector3.new(10, 4, 10))
 testMap:Build("test")
 
-print(testMap)
+local testMap2 = Map:New(Vector3.new(0, 0, 100), Vector3.new(10, 4, 10))
+testMap2:Build("test")
 
 task.wait(2)
-testMap:ShowDebug()
-
-task.wait(5)
-testMap:HideDebug()
+testMap:SetCell(1, 4, 10, Object.New("Water"))
+testMap2:SetCell(1, 4, 10, Object.New("Water"))
