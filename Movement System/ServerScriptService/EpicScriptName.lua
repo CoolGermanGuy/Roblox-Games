ce2e48998606fe397b57f493fc06555daea0263a
skid205 <EmailCGG@protonmail.com>
2024-09-26 16:35:43 +0200

    Update EpicScriptName.lua

--- Movement System/ServerScriptService/EpicScriptName.lua
+++ Movement System/ServerScriptService/EpicScriptName.lua
@@ -4,16 +4,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local Object = require(ReplicatedStorage:WaitForChild("Object"))
 local Map = require(ReplicatedStorage:WaitForChild("Map"))
 
--- Script
-local myMap = Map.New(Vector3.new(10,5,10))
--- test all kinds
-myMap:SetObject(1, 1, 1, Object:New("Grass", {}))
-myMap:SetObject(5, 2, 5, Object:New("Water", {}))
-
-myMap:SetObject(10, 5, 10, Object:New("Wall", {}))
--- test override
---myMap:SetObject(1, 1, 2, Object:New("Grass", {}))
-myMap:SetObject(1, 1, 3, Object:New("Grass", {["Transparency"] = 0.5}))
---myMap:SetObject(1, 1, 4, Object:New("Grass", {["Walkable"] = false}))
-
+local myMap = Map:New(Vector3.new(10,5,0))
 print(myMap)
+
+myMap:Build("test")
