c3dde6c8310b3c3014fa6e3f039047bcbedbca6b
skid205 <EmailCGG@protonmail.com>
2024-09-26 17:07:41 +0200

    Update Object.lua

--- Movement System/ReplicatedStorage/Object.lua
+++ Movement System/ReplicatedStorage/Object.lua
@@ -29,16 +29,14 @@ local ObjectKinds = {
 function Object.New(kind: string, override: {})
 	local newObject = {}
 	newObject.Kind = kind
-	newObject.Part = ObjectKinds[kind].Part:Clone()
+	newObject.Part = ObjectKinds[kind]["Part"]:Clone()
 	
 	-- loop to assign default or override values
-	for ObjectKind, ValueTable in ObjectKinds do
-		for Property in ValueTable do
-			if override and override[Property] ~= nil then -- if override has a property OR the part has a value
-				newObject[Property] = override[Property]
-			else
-				newObject[Property] = ObjectKinds[kind][Property] -- if it doesn't set it to default kind value
-			end
+	for Property in ObjectKinds[kind] do
+		if override and override[Property] ~= nil then -- if override has a property OR the part has a value
+			newObject[Property] = override[Property]
+		elseif Property ~= "Part" then
+			newObject[Property] = ObjectKinds[kind][Property] -- if it doesn't set it to default kind value
 		end
 	end
 	
