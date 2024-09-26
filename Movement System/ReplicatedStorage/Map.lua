fb9a716c93e254ac64a45163e383adcc20d0c7f8
skid205 <EmailCGG@protonmail.com>
2024-09-26 16:36:22 +0200

    Update Map.lua

--- Movement System/ReplicatedStorage/Map.lua
+++ Movement System/ReplicatedStorage/Map.lua
@@ -6,11 +6,11 @@ local Object = require(ReplicatedStorage:WaitForChild("Object"))
 -- Init
 local Map = {}
 Map.__index = Map
--- New Map
-function Map.New(size: Vector3)
+
+-- New
+function Map:New(size: Vector3)
 	local newMap = {}
-	size = size
-	
+	newMap.Size = size
 	-- init map
 	for x = 1, size.X do
 		newMap[x] = {}
@@ -18,10 +18,6 @@ function Map.New(size: Vector3)
 			newMap[x][y] = {}
 			for z = 1, size.Z do
 				newMap[x][y][z] = {}
-				newMap[x][y][z].Position = Vector3.new(size.X + (8*x), size.Y + (8*y), size.Z + (8*z))
-				--newMap[x][y][z].Part = Object.Init("Air", Vector3.new(size.X + (8*x), size.Y + (8*y), size.Z + (8*z)), {})
-				--newMap:SetObject(x, y, z, Object:New("Air", {}))
-				newMap[x][y][z].Part = Object:New("Air", {})
 			end
 		end
 	end
@@ -29,12 +25,27 @@ function Map.New(size: Vector3)
 	return newMap
 end
 
--- Functions
-function Map:SetObject(x: int, y: int, z: int, Object: Object)
-	print(self[x][y][z].Position)
-	Object.Position = self[x][y][z].Position
-	Object.Part.Position = Object.Position
-	self[x][y][z].Part = Object
+-- Methods
+function Map:GetPosition(x: int, y: int, z: int)
+	return Vector3.new(self.Size.X + (8*x), self.Size.Y + (8*y), self.Size.Z + (8*z))
+end
+
+function Map:SetCell(x: int, y: int, z: int, Object: Object)
+	self[x][y][z] = Object
+	Object.Part.Position = self:GetPosition(x, y, z)
+end
+
+function Map:Build(version: string)
+	if version == "test" then
+		-- big ass code bullshit
+		for i = 1, 10, 1 do
+			self:SetCell(i, 1, 1, Object.New("Grass"))
+		end
+		
+		-- bullshit end
+	else
+		print("yeah no")
+	end
 end
 
 return Map
