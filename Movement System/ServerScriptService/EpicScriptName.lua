05259d113c071df7ac48e7ba648a1b1f01f3b5d8
skid205 <EmailCGG@protonmail.com>
2024-09-30 18:13:39 +0200

    Update EpicScriptName.lua

--- Movement System/ServerScriptService/EpicScriptName.lua
+++ Movement System/ServerScriptService/EpicScriptName.lua
@@ -1,36 +1,32 @@
 -- Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
--- requirements
+local PlayersService = game:GetService("Players")
+-- Requirements
 local Object = require(ReplicatedStorage:WaitForChild("Object"))
 local Map = require(ReplicatedStorage:WaitForChild("Map"))
+local Player = require(ReplicatedStorage:WaitForChild("Player"))
+local GameHandler = require(ReplicatedStorage:WaitForChild("GameHandler"))
 
+-- Script
+local myGame = GameHandler.New()
 local testMap = Map:New(Vector3.new(0, 4, 0), Vector3.new(10, 4, 10))
 testMap:Build("test")
 testMap:ShowDebug()
 
-local neighbours = testMap:GetNeighboursAsDict(2, 2, 2)
-print(neighbours)
+myGame:AssignMap(testMap)
+
+---------------------
+local myGame2 = GameHandler.New()
+local testMap2 = Map:New(Vector3.new(0, 4, 100), Vector3.new(10, 4, 10))
+testMap2:Build("test")
+testMap2:ShowDebug()
+
+myGame2:AssignMap(testMap2)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-local map1 = Map:New(Vector3.new(0, 4, -16), Vector3.new(1, 1, 1))
-map1:Build("1")
-map1:ShowDebug()
-
-local map3 = Map:New(Vector3.new(0, 4, -48), Vector3.new(3, 3, 3))
-map3:Build("123")
-
-local maplayer = Map:New(Vector3.new(0, 4, -136), Vector3.new(10, 10, 10))
-maplayer:Build("layer")
+PlayersService.PlayerAdded:Connect(function(player)
+	local player = Player.New(player)
+	myGame:AddPlayer(player)
+	
+	print(player:GetGame())
+end)
