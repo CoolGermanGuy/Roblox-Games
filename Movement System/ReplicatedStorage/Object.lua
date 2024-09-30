435e3436c76578bddb7d75350568160191ac91be
skid205 <EmailCGG@protonmail.com>
2024-09-30 18:15:03 +0200

    Update Object.lua

--- Movement System/ReplicatedStorage/Object.lua
+++ Movement System/ReplicatedStorage/Object.lua
@@ -1,8 +1,12 @@
 -- Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
 -- Init
 local Object = {}
 Object.__index = Object
+
+-- Requirements
+local GameHandler = require(ReplicatedStorage:WaitForChild("GameHandler"))
 
 -- Object Kinds
 local ObjectKinds = {
@@ -35,9 +39,11 @@ function Object.New(kind: string, override: {})
 		end
 		kind = keys[math.random(#keys)]
 	end
+
+	newObject.GameIndex = 0
 	newObject.Kind = kind
 	newObject.Part = ObjectKinds[kind]["Part"]:Clone()
-	
+
 	-- loop to assign default or override values
 	for Property in ObjectKinds[kind] do
 		if override and override[Property] ~= nil then -- if override has a property OR the part has a value
@@ -46,14 +52,26 @@ function Object.New(kind: string, override: {})
 			newObject[Property] = ObjectKinds[kind][Property] -- if it doesn't set it to default kind value
 		end
 	end
-	
+
 	newObject.Part.Parent = workspace
 	setmetatable(newObject, Object)
 	return newObject
 end
 
--- Methods
+-- Functions/Methods
+--		Getters Start
+function Object:GetGame()
+	return GameHandler.GetGame(self.GameIndex)
+end
 
+function Object:GetMap()
+	return GameHandler:GetMap(self.GameIndex)
+end
+
+function Object:GetEnemies()
+	return GameHandler:GetEnemies(self.GameIndex)
+end
+--		Getters End
 
 
 return Object
