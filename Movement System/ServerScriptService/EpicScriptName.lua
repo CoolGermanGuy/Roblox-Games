415e406abfe2a4d4be882f19ed070402b47621dc
skid205 <EmailCGG@protonmail.com>
2024-09-30 19:02:52 +0200

    Update EpicScriptName.lua

--- Movement System/ServerScriptService/EpicScriptName.lua
+++ Movement System/ServerScriptService/EpicScriptName.lua
@@ -1,6 +1,7 @@
 -- Services
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local PlayersService = game:GetService("Players")
+
 -- Requirements
 local Object = require(ReplicatedStorage:WaitForChild("Object"))
 local Map = require(ReplicatedStorage:WaitForChild("Map"))
@@ -17,8 +18,8 @@ myGame:AssignMap(testMap)
 
 ---------------------
 local myGame2 = GameHandler.New()
-local testMap2 = Map:New(Vector3.new(0, 4, 100), Vector3.new(10, 4, 10))
-testMap2:Build("test")
+local testMap2 = Map:New(Vector3.new(0, 4, 100), Vector3.new(3, 3, 3))
+testMap2:Build("123")
 testMap2:ShowDebug()
 
 myGame2:AssignMap(testMap2)
@@ -26,7 +27,23 @@ myGame2:AssignMap(testMap2)
 
 PlayersService.PlayerAdded:Connect(function(player)
 	local player = Player.New(player)
+	player.Character.HumanoidRootPart.Anchored = true
 	myGame:AddPlayer(player)
-	
-	print(player:GetGame())
+	player.Position = {
+		x = 1,
+		y = 2,
+		z = 2
+	}
+	task.wait(2)
+	player:MoveTo(1, 2, 4)
+	task.wait(3)
+	player:Move("Front")
+	task.wait(1)
+	player:Move("Right")
+	task.wait(1)
+	player:Move("Front")
 end)
+
+local myObject = Object.New("Water")
+testMap:SetCell(1, 1, 1, myObject)
+print(myObject:GetNeighbours())
